e92a5b4409754ccecaf831f275e1fafbcdf2e1ec
claytonknittel <c.j.knittel@wustl.edu>
2020-08-28 21:29:41 -0500

    fix

--- include/util/util.h
+++ include/util/util.h
@@ -16,17 +16,7 @@ constexpr T & min(T a, T b) {
 }
 
 template<typename T>
-inline T & min(const T & a, const T & b) {
-    return a < b ? a : b;
-}
-
-template<typename T>
 constexpr T & max(T a, T b) {
-    return a < b ? b : a;
-}
-
-template<typename T>
-inline T & max(const T & a, const T & b) {
     return a < b ? b : a;
 }
 
