22f9c84a546aa96b5739e7e24d20d2f7276b4426
claytonknittel <c.j.knittel@wustl.edu>
2020-08-28 21:02:23 -0500

    update utils

--- include/util/util.h
+++ include/util/util.h
@@ -8,4 +8,40 @@
     fprintf((f), __FILE__ ":%d " fmt, __LINE__, __VA_ARGS__)
 
 
+#ifdef __cplusplus
+
+template<typename T>
+constexpr T & min(constexpr T a, constexpr T b) {
+    return a < b ? a : b;
+}
+
+template<typename T>
+inline T & min(const T & a, const T & b) {
+    return a < b ? a : b;
+}
+
+template<typename T>
+constexpr T & max(constexpr T a, constexpr T b) {
+    return a < b ? b : a;
+}
+
+template<typename T>
+inline T & max(const T & a, const T & b) {
+    return a < b ? b : a;
+}
+
+
+
+
+#else
+
+#define MIN(a, b) ((a) < (b) : (a) : (b))
+#define MAX(a, b) ((a) > (b) : (a) : (b))
+
+#define ALIGN_UP(val, algn) (((val) + ((algn) - 1)) & ~((algn) - 1))
+#define ALIGN_DOWN(val, algn) ((val) ~((algn) - 1))
+
+#endif /* __cplusplus */
+
+
 #endif /* _STDUTIL_H */
