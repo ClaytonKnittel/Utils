097e7bfaf28aba3bc27df46086fb1658cf319bee
claytonknittel <c.j.knittel@wustl.edu>
2020-10-28 03:20:13 -0500

    tested

--- test/rbtest_c.c
+++ test/rbtest_c.c
@@ -3,7 +3,6 @@
 
 
 int main(int argc, char * argv[]) {
-
     rb_tree tree;
 
     rb_init(&tree);
@@ -22,6 +21,33 @@ int main(int argc, char * argv[]) {
         rb_validate_ptr(&tree);
     }
 
+    for (int i = 0; i < N_NODES - 1; i++) {
+        assert(rb_find_succ(&nodes[i]) == &nodes[i + 1]);
+    }
+
+    for (int i = 1; i < N_NODES; i++) {
+        assert(rb_find_pred(&nodes[i]) == &nodes[i - 1]);
+    }
+
+    for (uint64_t addr = (uint64_t) &nodes[0];
+         addr < (uint64_t) &nodes[N_NODES];
+         addr += 8) {
+        uint64_t upper_bound = addr == ((uint64_t) &nodes[0])
+            ? 0
+            : ((uint64_t) &nodes[0])
+                + ((addr - ((uint64_t) &nodes[0]) - 1) / sizeof(rb_node))
+                    * sizeof(rb_node);
+        uint64_t lower_bound = addr > ((uint64_t) &nodes[N_NODES - 1])
+            ? 0
+            : ((uint64_t) &nodes[0])
+                + ((addr - ((uint64_t) &nodes[0]) + sizeof(rb_node) - 1)
+                      / sizeof(rb_node))
+                    * sizeof(rb_node);
+        assert(rb_upper_bound_ptr(&tree, (rb_node *) addr)
+            == (rb_node *) upper_bound);
+        assert(rb_lower_bound_ptr(&tree, (rb_node *) addr)
+            == (rb_node *) lower_bound);
+    }
+
     return 0;
 }
-
