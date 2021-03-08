da39ae8b003620dad9a710d4c1e4b578d881d3b3
claytonknittel <c.j.knittel@wustl.edu>
2021-03-07 20:42:38 -0600

    4x unrolled loop

--- src/string/to_upper_lower.s
+++ src/string/to_upper_lower.s
@@ -38,53 +38,73 @@ upper_to_lower:
 
 .text:
 _to_upper:
-    push rbx
+	test rsi, rsi
+	je _up_return
 
-	add rsi, rdi
+	push rbx
 
-	vmovdqa  ymm3, ymmword ptr upper_offset_array[rip]
-	vmovdqa  ymm4, ymmword ptr range_min[rip]
+	lea rbx, [rdi + rsi]
+	cmp rsi, 32
+	jb _up_remainder_loop
 
-_up_loop_init:
-	mov rax, rdi
-	add rax, 31
-	and rax, 0xffffffffffffffe0
+	vmovdqa  ymm0, ymmword ptr upper_offset_array[rip]
+	vmovdqa  ymm1, ymmword ptr range_min[rip]
+	vmovdqa  ymm2, ymmword ptr lower_to_upper[rip]
 
-	# calculate the upper bound and place it in rcx
-	cmp rsi, rax
-	mov rcx, rax
-	cmovb rcx, rsi
-
-	jmp _up_beg_loop_cond
-
-_up_beg_loop:
-	movzx eax, byte ptr [rdi]
-	lea ebx, [rax - 0x61]
-	lea edx, [rax - 0x20]
-	cmp ebx, 26
-	cmovb eax, edx
-	mov byte ptr [rdi], al
-
-	inc rdi
-_up_beg_loop_cond:
-	cmp rdi, rcx
-	jne _up_beg_loop
-
-_up_vector_loop_init:
 	mov rcx, rsi
 	and rcx, 0xffffffffffffffe0
+	add rcx, rdi
+
+	cmp rsi, 128
+	jb _up_vector_loop
+
+	mov rdx, rsi
+	and rdx, 0xffffffffffffff80
+	add rdx, rdi
+
+_up_4x_vector_loop:
+	vmovdqu  ymm3, ymmword ptr [rdi]
+	vmovdqu  ymm5, ymmword ptr [rdi + 32]
+	vmovdqu  ymm7, ymmword ptr [rdi + 64]
+	vmovdqu  ymm9, ymmword ptr [rdi + 96]
+	vpaddb   ymm4,  ymm3,  ymm0
+	vpaddb   ymm6,  ymm5,  ymm0
+	vpaddb   ymm8,  ymm7,  ymm0
+	vpaddb   ymm10, ymm9,  ymm0
+	vpcmpgtb ymm4,  ymm4,  ymm1
+	vpcmpgtb ymm6,  ymm6,  ymm1
+	vpcmpgtb ymm8,  ymm8,  ymm1
+	vpcmpgtb ymm10, ymm10, ymm1
+	vpand    ymm4,  ymm4,  ymm2
+	vpand    ymm6,  ymm6,  ymm2
+	vpand    ymm8,  ymm8,  ymm2
+	vpand    ymm10, ymm10, ymm2
+	vpaddb   ymm3,  ymm4,  ymm3
+	vpaddb   ymm5,  ymm6,  ymm5
+	vpaddb   ymm7,  ymm8,  ymm7
+	vpaddb   ymm9,  ymm10, ymm9
+	vmovdqu  ymmword ptr [rdi],      ymm3
+	vmovdqu  ymmword ptr [rdi + 32], ymm5
+	vmovdqu  ymmword ptr [rdi + 64], ymm7
+	vmovdqu  ymmword ptr [rdi + 96], ymm9
+
+	add rdi, 128
+
+_up_4x_vector_loop_cond:
+	cmp rdi, rdx
+	jne _up_4x_vector_loop
 
 _up_vector_loop_cond_1:
 	cmp rdi, rcx
-	jae _up_remainder_loop_cond_1
+	je _up_remainder_loop_cond_2
 
 _up_vector_loop:
-	vmovdqa  ymm1, ymmword ptr [rdi]
-	vpaddb   ymm2, ymm3, ymm1
-	vpcmpgtb ymm2, ymm2, ymm4
-	vpand    ymm2, ymm2, ymmword ptr lower_to_upper[rip]
-	vpaddb   ymm1, ymm2, ymm1
-	vmovdqa  ymmword ptr [rdi], ymm1
+	vmovdqu  ymm3, ymmword ptr [rdi]
+	vpaddb   ymm4, ymm3, ymm0
+	vpcmpgtb ymm4, ymm4, ymm1
+	vpand    ymm4, ymm4, ymm2
+	vpaddb   ymm3, ymm4, ymm3
+	vmovdqu  ymmword ptr [rdi], ymm3
 
 	add rdi, 32
 
@@ -93,28 +113,129 @@ _up_vector_loop_cond_2:
 	jb _up_vector_loop
 
 _up_remainder_loop_cond_1:
-	cmp rdi, rsi
+	cmp rdi, rbx
 	je _up_finished
 
 _up_remainder_loop:
 	movzx eax, byte ptr [rdi]
-	lea ebx, [rax - 0x61]
+	lea ecx, [rax - 0x61]
 	lea edx, [rax - 0x20]
-	cmp ebx, 26
+	cmp ecx, 26
 	cmovb eax, edx
 	mov byte ptr [rdi], al
 
 	inc rdi
 
 _up_remainder_loop_cond_2:
-	cmp rdi, rsi
+	cmp rdi, rbx
 	jne _up_remainder_loop
 
 _up_finished:
 	pop rbx
+_up_return:
 	ret
 
 
+_to_lower:
+	test rsi, rsi
+	je _lo_return
+
+	push rbx
+
+	lea rbx, [rdi + rsi]
+	cmp rsi, 32
+	jb _lo_remainder_loop
+
+	vmovdqa  ymm0, ymmword ptr lower_offset_array[rip]
+	vmovdqa  ymm1, ymmword ptr range_min[rip]
+	vmovdqa  ymm2, ymmword ptr upper_to_lower[rip]
+
+	mov rcx, rsi
+	and rcx, 0xffffffffffffffe0
+	add rcx, rdi
+
+	cmp rsi, 128
+	jb _lo_vector_loop
+
+	mov rdx, rsi
+	and rdx, 0xffffffffffffff80
+	add rdx, rdi
+
+_lo_4x_vector_loop:
+	vmovdqu  ymm3, ymmword ptr [rdi]
+	vmovdqu  ymm5, ymmword ptr [rdi + 32]
+	vmovdqu  ymm7, ymmword ptr [rdi + 64]
+	vmovdqu  ymm9, ymmword ptr [rdi + 96]
+	vpaddb   ymm4,  ymm3,  ymm0
+	vpaddb   ymm6,  ymm5,  ymm0
+	vpaddb   ymm8,  ymm7,  ymm0
+	vpaddb   ymm10, ymm9,  ymm0
+	vpcmpgtb ymm4,  ymm4,  ymm1
+	vpcmpgtb ymm6,  ymm6,  ymm1
+	vpcmpgtb ymm8,  ymm8,  ymm1
+	vpcmpgtb ymm10, ymm10, ymm1
+	vpand    ymm4,  ymm4,  ymm2
+	vpand    ymm6,  ymm6,  ymm2
+	vpand    ymm8,  ymm8,  ymm2
+	vpand    ymm10, ymm10, ymm2
+	vpaddb   ymm3,  ymm4,  ymm3
+	vpaddb   ymm5,  ymm6,  ymm5
+	vpaddb   ymm7,  ymm8,  ymm7
+	vpaddb   ymm9,  ymm10, ymm9
+	vmovdqu  ymmword ptr [rdi],      ymm3
+	vmovdqu  ymmword ptr [rdi + 32], ymm5
+	vmovdqu  ymmword ptr [rdi + 64], ymm7
+	vmovdqu  ymmword ptr [rdi + 96], ymm9
+
+	add rdi, 128
+
+_lo_4x_vector_loop_cond:
+	cmp rdi, rdx
+	jne _lo_4x_vector_loop
+
+_lo_vector_loop_cond_1:
+	cmp rdi, rcx
+	je _lo_remainder_loop_cond_2
+
+_lo_vector_loop:
+	vmovdqu  ymm3, ymmword ptr [rdi]
+	vpaddb   ymm4, ymm3, ymm0
+	vpcmpgtb ymm4, ymm4, ymm1
+	vpand    ymm4, ymm4, ymm2
+	vpaddb   ymm3, ymm4, ymm3
+	vmovdqu  ymmword ptr [rdi], ymm3
+
+	add rdi, 32
+
+_lo_vector_loop_cond_2:
+	cmp rdi, rcx
+	jb _lo_vector_loop
+
+_lo_remainder_loop_cond_1:
+	cmp rdi, rbx
+	je _lo_finished
+
+_lo_remainder_loop:
+	movzx eax, byte ptr [rdi]
+	lea ecx, [rax - 0x41]
+	lea edx, [rax + 0x20]
+	cmp ecx, 26
+	cmovb eax, edx
+	mov byte ptr [rdi], al
+
+	inc rdi
+
+_lo_remainder_loop_cond_2:
+	cmp rdi, rbx
+	jne _lo_remainder_loop
+
+_lo_finished:
+	pop rbx
+_lo_return:
+	ret
+
+
+/*
 _to_lower:
     push rbx
 
@@ -191,4 +312,4 @@ _lo_remainder_loop_cond_2:
 _lo_finished:
 	pop rbx
 	ret
-
+*/
--- test/to_upper_lower_test.c
+++ test/to_upper_lower_test.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include <string/string.h>
 
@@ -20,9 +21,6 @@ void gen_rand_str(char* rand_str, uint64_t len) {
 
 void __attribute__((noinline)) naive_to_upper(char* s, uint64_t len) {
 	for (uint64_t i = 0; i < len; i++) {
-		/*if (s[i] >= 'a' && s[i] <= 'z') {
-			s[i] += 'A' - 'a';
-		}*/
 		int8_t rotated = s[i];
 		rotated += 0x05;
 		rotated &= 0x7f;
@@ -36,9 +34,6 @@ void __attribute__((noinline)) naive_to_upper(char* s, uint64_t len) {
 
 void __attribute__((noinline)) naive_to_lower(char* s, uint64_t len) {
 	for (uint64_t i = 0; i < len; i++) {
-		/*if (s[i] >= 'A' && s[i] <= 'Z') {
-			s[i] += 'a' - 'A';
-		}*/
 		uint8_t rotated = s[i];
 		rotated += 0x25;
 		rotated &= 0x7f;
@@ -54,8 +49,8 @@ void __attribute__((noinline)) naive_to_lower(char* s, uint64_t len) {
  * calculates the average amount of time each function takes to capitalize
  * random strings of the given length
  */
-void calc_avg_times_upper(uint64_t len, uint32_t n_trials, float* naive_s,
-		float* my_s, bool verify, uint64_t rand_offset_amt) {
+void calc_avg_times_upper(uint64_t len, uint32_t n_trials, double* naive_s,
+		double* my_s, bool verify, uint64_t rand_offset_amt) {
 	char* _str = (char*) malloc(len + 1 + rand_offset_amt);
 	char* _str1 = (char*) malloc(len + 1 + rand_offset_amt);
 	char* _str2 = (char*) malloc(len + 1 + rand_offset_amt);
@@ -129,8 +124,8 @@ void calc_avg_times_upper(uint64_t len, uint32_t n_trials, float* naive_s,
 		}
 	}
 
-	*naive_s = (float) (((double) total_ns_1) / (1000000000. * (double) n_trials));
-	*my_s = (float) (((double) total_ns_2) / (1000000000. * (double) n_trials));
+	*naive_s = ((double) total_ns_1) / (1000000000. * (double) n_trials);
+	*my_s = ((double) total_ns_2) / (1000000000. * (double) n_trials);
 
 	free(_str1);
 	free(_str2);
@@ -140,8 +135,8 @@ void calc_avg_times_upper(uint64_t len, uint32_t n_trials, float* naive_s,
  * calculates the average amount of time each function takes to capitalize
  * random strings of the given length
  */
-void calc_avg_times_lower(uint64_t len, uint32_t n_trials, float* naive_s,
-		float* my_s, bool verify, uint64_t rand_offset_amt) {
+void calc_avg_times_lower(uint64_t len, uint32_t n_trials, double* naive_s,
+		double* my_s, bool verify, uint64_t rand_offset_amt) {
 	char* _str = (char*) malloc(len + 1 + rand_offset_amt);
 	char* _str1 = (char*) malloc(len + 1 + rand_offset_amt);
 	char* _str2 = (char*) malloc(len + 1 + rand_offset_amt);
@@ -215,8 +210,8 @@ void calc_avg_times_lower(uint64_t len, uint32_t n_trials, float* naive_s,
 		}
 	}
 
-	*naive_s = (float) (((double) total_ns_1) / (1000000000. * (double) n_trials));
-	*my_s = (float) (((double) total_ns_2) / (1000000000. * (double) n_trials));
+	*naive_s = ((double) total_ns_1) / (1000000000. * (double) n_trials);
+	*my_s = ((double) total_ns_2) / (1000000000. * (double) n_trials);
 
 	free(_str1);
 	free(_str2);
@@ -224,18 +219,18 @@ void calc_avg_times_lower(uint64_t len, uint32_t n_trials, float* naive_s,
 
 int main() {
 	srand(time(NULL));
-	float naive_s, my_s;
+	double naive_s, my_s;
 
-	uint64_t max = 20;
-	printf("{");
+	uint64_t max = 200;
+	//printf("{");
 
 	int sz = max;
 	//for (int sz = 1; sz < max; sz++) {
-		calc_avg_times_upper(sz, 2000000, &naive_s, &my_s, true, 0);
+		calc_avg_times_upper(sz, 200000, &naive_s, &my_s, true, 0);
 
 		printf("upper:\n");
-		printf("average naive: %f\n", naive_s);
-		printf("average mine:  %f\n", my_s);
+		printf("average naive: %.9f\n", naive_s);
+		printf("average mine:  %.9f\n", my_s);
 		printf("speedup:       %f\n", naive_s / my_s);
 
 		/*printf("{%d, %f}", sz, (naive_s / my_s));
@@ -243,16 +238,17 @@ int main() {
 			printf(",");
 		}
 		fflush(stdout);*/
+		usleep(10000);
 
-		calc_avg_times_lower(sz, 2000000, &naive_s, &my_s, true, 0);
+		calc_avg_times_lower(sz, 200000, &naive_s, &my_s, true, 0);
 
 		printf("lower:\n");
-		printf("average naive: %f\n", naive_s);
-		printf("average mine:  %f\n", my_s);
+		printf("average naive: %.9f\n", naive_s);
+		printf("average mine:  %.9f\n", my_s);
 		printf("speedup:       %f\n", naive_s / my_s);
 
 	//}
-	printf("}\n");
+	//printf("}\n");
 	return 0;
 }
 
