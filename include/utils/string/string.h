dbb93ae14022f28e683ce9b6415d52b21b6ebc8a
claytonknittel <cknittel@wustl.edu>
2022-05-06 00:39:52 -0700

    fallback to_upper/lower if no AVX

--- include/utils/string/string.h
+++ include/utils/string/string.h
@@ -8,13 +8,28 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#ifdef __APPLE__
-extern void to_upper_osx(char* str, uint64_t len);
-extern void to_lower_osx(char* str, uint64_t len);
+#ifdef AVX_SUPPORTED
+extern void to_upper_avx(char* str, uint64_t len);
+extern void to_lower_avx(char* str, uint64_t len);
 
-#define to_upper to_upper_osx
-#define to_lower to_lower_osx
-#endif
+#define to_upper to_upper_avx
+#define to_lower to_lower_avx
+
+#else
+
+static void to_upper(char* s, uint64_t len) {
+	for (uint64_t i = 0; i < len; i++) {
+		s[i] = (s[i] >= 'a' && s[i] <= 'z') ? (s[i] + ('A' - 'a')) : s[i];
+	}
+}
+
+static void to_lower(char* s, uint64_t len) {
+	for (uint64_t i = 0; i < len; i++) {
+		s[i] = (s[i] >= 'A' && s[i] <= 'Z') ? (s[i] + ('a' - 'A')) : s[i];
+	}
+}
+
+#endif /* AVX_SUPPORTED */
 
 #ifdef __cplusplus
 }
