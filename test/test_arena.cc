2a34df919d4044b7eab5d296aa2a940643e61af0
claytonknittel <cknittel@wustl.edu>
2022-10-30 15:43:20 -0700

    test misaligned arena elements

--- test/test_arena.cc
+++ test/test_arena.cc
@@ -1,13 +1,15 @@
 
-#include <array>
+#include <vector>
 
+#include <utils/math/random.h>
 #include <utils/memory/arena.h>
 
 #include "test_utils.h"
 
 using namespace util::memory;
+using util::Random;
 
-START_TEST(test_one) {
+START_TEST(test_uint64_one) {
   Arena<uint64_t> a;
   uint64_t* start = a.start();
 
@@ -18,53 +20,100 @@ START_TEST(test_one) {
 }
 END_TEST
 
-template <uint32_t n_pages>
-static void test_uint64_pages() {
-  constexpr uint64_t n_ptrs = n_pages * 256 * 1024;
-  Arena<uint64_t> a;
-  uint64_t* start = a.start();
-  std::array<ArenaPtr<uint64_t>, n_ptrs> ptr_list;
+// T must have a constructor T(uint64_t) and define operator==
+template <class T>
+static void test_uint64_pages(uint32_t n_pages) {
+  uint64_t n_ptrs = n_pages * 2 * 1024 * 1024 / sizeof(T);
+  Arena<T> a;
+  const T* start = a.start();
+  std::vector<ArenaPtr<T>> ptr_list;
+  ptr_list.reserve(n_ptrs);
 
   for (uint64_t i = 0; i < n_ptrs; i++) {
-    ArenaPtr<uint64_t> ptr = a.malloc(i);
+    ArenaPtr<T> ptr = a.malloc(i);
+    ck_assert(ptr != ArenaPtr<T>::null());
     ck_assert_ptr_eq(ptr.get(a), start + i);
-    ck_assert_uint_eq(*ptr.get(a), i);
+    ck_assert(*ptr.get(a) == T(i));
 
-    ptr_list[i] = ptr;
+    ptr_list.push_back(std::move(ptr));
   }
 
   for (uint64_t i = 0; i < n_ptrs; i++) {
-    ck_assert_uint_eq(*ptr_list[i].get(a), i);
+    ck_assert(ptr_list[i] != ArenaPtr<T>::null());
+    ck_assert_ptr_eq(ptr_list[i].get(a), start + i);
+    ck_assert(*ptr_list[i].get(a) == T(i));
   }
 }
 
-START_TEST(test_single_page) {
-  test_uint64_pages<1>();
+START_TEST(test_uint64_single_page) {
+  test_uint64_pages<uint64_t>(1);
 }
 END_TEST
 
-START_TEST(test_two_pages) {
-  test_uint64_pages<2>();
+START_TEST(test_uint64_two_pages) {
+  test_uint64_pages<uint64_t>(2);
 }
 END_TEST
 
-START_TEST(test_ten_pages) {
-  test_uint64_pages<10>();
+START_TEST(test_uint64_ten_pages) {
+  test_uint64_pages<uint64_t>(10);
+}
+END_TEST
+
+class Misaligned {
+ private:
+  uint16_t b_[19];
+
+ public:
+  Misaligned(uint64_t i) {
+    (void) i;
+    for (uint64_t i = 0; i < 19; i++) {
+      b_[i] = static_cast<uint16_t>(i);
+    }
+  }
+
+  bool operator==(const Misaligned& other) {
+    return memcmp(b_, other.b_, sizeof(b_)) == 0;
+  }
+};
+
+static_assert(sizeof(Misaligned) == 38);
+
+START_TEST(test_misaligned_single_page) {
+  test_uint64_pages<Misaligned>(1);
+}
+END_TEST
+
+START_TEST(test_misaligned_two_pages) {
+  test_uint64_pages<Misaligned>(2);
+}
+END_TEST
+
+START_TEST(test_misaligned_ten_pages) {
+  test_uint64_pages<Misaligned>(10);
 }
 END_TEST
 
 Suite* test_arena_cc() {
   TCase* tc_simple;
+  TCase* tc_misaligned;
 
   Suite* s = suite_create("Arena");
 
   tc_simple = tcase_create("Simple");
   tcase_set_timeout(tc_simple, 0);
-  tcase_add_test(tc_simple, test_one);
-  tcase_add_test(tc_simple, test_single_page);
-  tcase_add_test(tc_simple, test_two_pages);
-  tcase_add_test(tc_simple, test_ten_pages);
+  tcase_add_test(tc_simple, test_uint64_one);
+  tcase_add_test(tc_simple, test_uint64_single_page);
+  tcase_add_test(tc_simple, test_uint64_two_pages);
+  tcase_add_test(tc_simple, test_uint64_ten_pages);
   suite_add_tcase(s, tc_simple);
+
+  tc_misaligned = tcase_create("Misaligned");
+  tcase_set_timeout(tc_misaligned, 0);
+  tcase_add_test(tc_misaligned, test_misaligned_single_page);
+  tcase_add_test(tc_misaligned, test_misaligned_two_pages);
+  tcase_add_test(tc_misaligned, test_misaligned_ten_pages);
+  suite_add_tcase(s, tc_misaligned);
 
   return s;
 }
