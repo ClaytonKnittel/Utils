2326b8477b4bdba5daf134187e5015b0bb9baf59
claytonknittel <cknittel@wustl.edu>
2022-10-30 15:15:59 -0700

    variable ArenaPtr sizes

--- include/utils/memory/arena.h
+++ include/utils/memory/arena.h
@@ -17,45 +17,53 @@ namespace util {
 
 namespace memory {
 
-template <class T>
+template <class T, uint32_t ptr_size>
 class Arena;
 
-template <class T>
+template <class T, uint32_t ptr_size = 8>
 class ArenaPtr {
-  template <class _T>
+  template <class _T, uint32_t _ptr_size>
   friend class Arena;
 
+  typedef typename std::conditional<
+      ptr_size == 8, uint64_t,
+      std::conditional<
+          ptr_size == 4, uint32_t,
+          std::conditional<ptr_size == 2, uint16_t,
+                           std::conditional<ptr_size == 1, uint8_t, void>>>>::
+      type ptr_data_t;
+
  private:
-  constexpr ArenaPtr(T* ptr, const Arena<T>& arena);
+  constexpr ArenaPtr(T* ptr, const Arena<T, ptr_size>& arena);
 
   static constexpr uint64_t PtrToInt(T* ptr, T* arena_start) {
     return static_cast<uint64_t>(ptr - arena_start);
   }
 
  public:
-  explicit constexpr ArenaPtr(uint32_t ptr_val) : ptr_val_(ptr_val) {}
+  explicit constexpr ArenaPtr(uint64_t ptr_val) : ptr_val_(ptr_val) {}
   constexpr ArenaPtr(const ArenaPtr& ptr) = default;
   constexpr ArenaPtr() : ArenaPtr(null()) {}
 
-  bool operator==(const ArenaPtr<T>& other) {
+  bool operator==(const ArenaPtr<T, ptr_size>& other) {
     return ptr_val_ == other.ptr_val_;
   }
 
-  bool operator!=(const ArenaPtr<T>& other) {
+  bool operator!=(const ArenaPtr<T, ptr_size>& other) {
     return !(*this == other);
   }
 
-  T* get(const Arena<T>& arena) const;
+  T* get(const Arena<T, ptr_size>& arena) const;
 
-  static constexpr ArenaPtr<T> null() {
-    return ArenaPtr<T>(-1u);
+  static constexpr ArenaPtr<T, ptr_size> null() {
+    return ArenaPtr<T, ptr_size>(-1u);
   }
 
  private:
-  uint32_t ptr_val_;
+  ptr_data_t ptr_val_;
 };
 
-template <class T>
+template <class T, uint32_t ptr_size = 8>
 class Arena {
  private:
   static constexpr int MMAP_FLAGS =
@@ -107,17 +115,17 @@ class Arena {
   }
 
   template <typename... Args>
-  ArenaPtr<T> malloc(Args... args) {
+  ArenaPtr<T, ptr_size> malloc(Args... args) {
     if (next_ + 1 > end_) {
       void* res = SystemMemMapExtend(end_, arena_size_);
       if (res == nullptr) {
-        return ArenaPtr<T>::null();
+        return ArenaPtr<T, ptr_size>::null();
       }
 
       end_ =
           reinterpret_cast<T*>(reinterpret_cast<uint64_t>(end_) + arena_size_);
     }
-    ArenaPtr<T> res(next_, *this);
+    ArenaPtr<T, ptr_size> res(next_, *this);
 
     // Construct the element in place.
     new (reinterpret_cast<void*>(next_)) T(std::forward<Args>(args)...);
@@ -133,12 +141,13 @@ class Arena {
   std::size_t arena_size_;
 };
 
-template <class T>
-constexpr ArenaPtr<T>::ArenaPtr(T* ptr, const Arena<T>& arena)
-    : ptr_val_(PtrToInt(ptr, arena.start())) {}
+template <class T, uint32_t ptr_size>
+constexpr ArenaPtr<T, ptr_size>::ArenaPtr(T* ptr,
+                                          const Arena<T, ptr_size>& arena)
+    : ArenaPtr(PtrToInt(ptr, arena.start())) {}
 
-template <class T>
-T* ArenaPtr<T>::get(const Arena<T>& arena) const {
+template <class T, uint32_t ptr_size>
+T* ArenaPtr<T, ptr_size>::get(const Arena<T, ptr_size>& arena) const {
   return arena.start() + ptr_val_;
 }
 
